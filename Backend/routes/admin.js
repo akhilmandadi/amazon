1c978ef45912aadc0ceb1ba78af2fa03feaafd50
Vamsi-Mundra <60533855+Vamsi-Mundra@users.noreply.github.com>
2020-04-26 18:11:33 -0700

    Admin routes backend

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -24,4 +24,87 @@ router.get('/sellers', async (request, response) => {
     }
 });
 
+router.get('/allCategories', async (request, response) => {
+    try {
+        const data = {
+            "params": request.params,
+            "query": request.query,
+            "type": "getAllCategories"
+        }
+        await kafka.make_request('admin', data, function (err, data) {
+            if (err) throw new Error(err)
+            response.status(data.status).json(data.body);
+        });
+    } catch (ex) {
+        logger.error(ex);
+        const message = ex.message ? ex.message : 'Error while fetching seller details';
+        const code = ex.statusCode ? ex.statusCode : 500;
+        return response.status(code).json({ message });
+    }
+});
+
+router.post('/category',async (request,response)=>{
+    try {
+        const data = {
+            "body": request.body,
+            "params": request.params,
+            "query": request.query,
+            "type": "saveCategory"
+        }
+        await kafka.make_request('admin', data, function (err, data) {
+            if (err) throw new Error(err)
+            response.status(data.status).json(data.body);
+        });
+    } catch (ex) {
+        logger.error(ex);
+        const message = ex.message ? ex.message : 'Error while fetching orders';
+        const code = ex.statusCode ? ex.statusCode : 500;
+        return response.status(code).json({ message });
+    }
+})
+
+router.delete('/category/:id',async (request,response)=>{
+    try {
+        const data = {
+            "body": request.body,
+            "params": request.params,
+            "query": request.query,
+            "type": "removeCategory"
+        }
+        await kafka.make_request('admin', data, function (err, data) {
+            if (err) throw new Error(err)
+            response.status(data.status).json(data.body);
+        });
+    } catch (ex) {
+        logger.error(ex);
+        const message = ex.message ? ex.message : 'Error while fetching orders';
+        const code = ex.statusCode ? ex.statusCode : 500;
+        return response.status(code).json({ message });
+    }
+})
+
+router.get('/admin/products', async (request, response) => {
+    try {
+      console.log(request.query)
+      const data = {
+        "body": request.body,
+        "params": request.params,
+        "query": request.query,
+        "type": "fetchAdminProducts"
+      }
+      await kafka.make_request('admin', data, function (err, data){
+        if (err) throw new Error(err)
+        response.status(data.status).json(data.body);
+      });
+    } catch (ex) {
+      logger.error(ex);
+      const message = ex.message ? ex.message : 'Error while fetching Products';
+      const code = ex.statusCode ? ex.statusCode : 500;
+      return response.status(code).json({ message });
+    }
+  });
+
+
+
+
 module.exports = router;
