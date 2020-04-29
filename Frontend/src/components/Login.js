88e0b9f5b6fcd45c02f1e594a35d9ceee985534b
Akhil Reddy <akhilreddymandadi@gmail.com>
2020-04-28 20:58:50 -0700

    Login, Signup Styling, Removed Navbar for Auth Pages

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -57,83 +57,70 @@ class Login extends Component {
             redirectVar = <Redirect to={"/admin/home"} />
         }
         return (
-            <div>
+            <div class="container">
                 {redirectVar}
-                <div class="row" style={{"padding-bottom":"20px","padding-left":"50px"}}>
-                <div class="col-lg-5"></div>
-                <div class="col-lg-2">
-
-                   <div >
-                             <img src={Amazon} style={{ width: "100px" }} />
-                       
-                    </div>
-                    </div>
-                    </div>
-                    <div class="row" style={{"padding-left":"80px"}}>
+                <div class="row" style={{ "padding": "10px 0px 10px", textAlign: "center" }}>
+                    <img src={Amazon} style={{ width: "120px" }} />
+                </div>
+                <div class="row" style={{}}>
                     <div class="col-lg-4"></div>
                     <div class="col-lg-2">
-                <div class="outer1container">
-               
-
-                    <div class="row">
-                        <div class="col-lg-10 col-xl-9 mx-auto">
-                            <div class="card card-signin flex-row my-5">
-                                <div class="card-body">
-                                    <form onSubmit={this.onSubmit} class="loginform">
-                                        <h2 class="card-title text-left"> Sign-In</h2>
-                                        <form>
-                                            <div className="radio-inline">
-                                                <label>
-                                                    <input type="radio" value="customer" checked={this.state.persona === 'customer'} onChange={this.handleOptionChange} />
+                        <div class="outer1container">
+                            <div class="row">
+                                <div class="col-lg-10 col-xl-9 mx-auto">
+                                    <div class="card card-signin flex-row my-5">
+                                        <div class="card-body">
+                                            <form onSubmit={this.onSubmit} class="loginform" style={{marginTop:"30px"}}>
+                                                <h2 class="card-title text-left"> Sign-In</h2>
+                                                <form style={{marginTop:"20px"}}>
+                                                    <div className="radio-inline">
+                                                        <label>
+                                                            <input type="radio" value="customer" checked={this.state.persona === 'customer'} onChange={this.handleOptionChange} />
                                                              Customer
-                                                 </label>
-                                            </div>
-                                            <div className="radio-inline">
-                                                <label>
-                                                    <input type="radio" value="seller" checked={this.state.persona === 'seller'} onChange={this.handleOptionChange} />
+                                                        </label>
+                                                    </div>
+                                                    <div className="radio-inline">
+                                                        <label>
+                                                            <input type="radio" value="seller" checked={this.state.persona === 'seller'} onChange={this.handleOptionChange} />
                                                                Seller
-                                                </label>
-                                            </div>
-                                            <div className="radio-inline">
-                                                <label>
-                                                    <input type="radio" value="admin" checked={this.state.persona === 'admin'} onChange={this.handleOptionChange} />
-                                                    Admin
-                                                </label>
-                                            </div>
-                                        </form>
-                                        <br></br>
-
-                                        <div class="form-group">
-                                            <label for="mail" class="form_label">
-                                                Email
-                                            </label>
-                                            <input type="email" class="form-control" name="mail" onChange={this.onChange} pattern="^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$'%&*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])$" title="Please enter valid email address" required />
+                                                        </label>
+                                                    </div>
+                                                    <div className="radio-inline">
+                                                        <label>
+                                                            <input type="radio" value="admin" checked={this.state.persona === 'admin'} onChange={this.handleOptionChange} />
+                                                            Admin
+                                                        </label>
+                                                    </div>
+                                                </form>
+                                                <br></br>
+                                                <div class="form-group">
+                                                    <label for="mail" class="form_label">
+                                                        Email
+                                                    </label>
+                                                    <input type="email" class="form-control" name="mail" onChange={this.onChange} pattern="^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$'%&*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])$" title="Please enter valid email address" required />
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="password" class="form_label">
+                                                        Password
+                                                    </label>
+                                                    <input type="password" class="form-control" name="password" onChange={this.onChange} required />
+                                                </div>
+                                                <div class="form-group" style={{ "alignItems": "center" }}>
+                                                    {this.props.invalidCredentials ? <span style={{ color: "red", "font-style": "oblique", "font-weight": "bold", "textAlign": "center" }}>Invalid Credentials</span> : ''}
+                                                </div>
+                                                <button type="submit" disabled={this.validateCredentials()} class="login-button">
+                                                    Sign-In
+                                                </button>
+                                                <br />
+                                                <div style={{textAlign:"center"}}><br/> Create your Amazon account? <Link to='/signup'>Signup</Link></div>
+                                            </form>
                                         </div>
-
-                                        <div class="form-group">
-                                            <label for="password" class="form_label">
-                                                Password
-                                            </label>
-                                            <input type="password" class="form-control" name="password" onChange={this.onChange} required />
-                                        </div>
-                                        <div class="form-group" style={{ "alignItems": "center" }}>
-                                            {this.props.invalidCredentials ? <span style={{ color: "red", "font-style": "oblique", "font-weight": "bold", "textAlign": "center" }}>Invalid Credentials</span> : ''}
-                                        </div>
-
-                                        <button type="submit" disabled={this.validateCredentials()} class="login-button">
-                                            Sign-In
-                                        </button>
-                                        <br /><br />
-                                        <div><br></br> Create your Amazon account? <Link to='/signup'>Signup</Link></div>
-
-                                    </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                </div>
-            </div>
             </div>
         )
     }
