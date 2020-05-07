3d2286f844389a268eec829769c13c6b1617a063
jagan695 <jagan.gadamsetty@sjsu.edu>
2020-05-07 00:09:30 -0700

    Frontend Fixes.

--- Frontend/src/redux/reducers/cartReducer.js
+++ Frontend/src/redux/reducers/cartReducer.js
@@ -61,7 +61,8 @@ export default function (state = initialState, action) {
                 cartlist: action.payload,
                 cartsubtotal: subtotal.toFixed(2),
                 carttotalitems: _.sumBy(action.payload, 'quantity'),
-                cartRedirect: false  
+                cartRedirect: false,
+                clickedProductDetails:""  
             });
         case CUSTOMER_CHECKOUT_DETAILS:
             return Object.assign({}, state, {
@@ -74,6 +75,7 @@ export default function (state = initialState, action) {
                 checkoutdetails:action.payload[2]
             });
         case CUSTOMER_ORDER_SUMMARY:
+            sessionStorage.setItem("cartCount",0)
             return Object.assign({}, state, {
                 ordersummary: action.payload,
                 orderflag : true,
